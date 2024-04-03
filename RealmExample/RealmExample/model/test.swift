b2e7f2e795439016ba62701b8b77c0bbfb815459
wyl <920048449@qq.com>
2024-04-03 09:51:47 +0800

    测试删除

---
+++ RealmExample/RealmExample/model/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  RealmExample
+//
+//  Created by 王延磊 on 2024/4/3.
+//
+
+import UIKit
+
+class test: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
